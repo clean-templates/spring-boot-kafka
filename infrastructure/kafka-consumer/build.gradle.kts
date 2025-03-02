826bf7bc94b484b82af6f417e1fc66ad767d90bd
RolandSall <roland.salloum00@outlook.com>
2025-03-02 15:51:18 +0200

    feat: kafka consumer

--- infrastructure/kafka-consumer/build.gradle.kts
+++ infrastructure/kafka-consumer/build.gradle.kts
@@ -7,3 +7,9 @@ plugins {
 group = "com.rolandsall"
 version = "unspecified"
 
+group = "com.rolandsall"
+version = "unspecified"
+
+dependencies {
+    implementation(project(":infrastructure:kafka-config-data"))
+}
